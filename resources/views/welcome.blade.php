b327fb475b43d9ef890b8ce0933512127d823ef2
Victor Mongi <victor.mongi@gmail.com>
2018-11-21 21:39:52 +0800

    Pindah Repo

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,52 +1,49 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Insightful Community | ICON</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
+    <title>Insightful Community | ICON</title>
 
-        <!-- Styles -->
-	<style>
-		.title{
-	display: flex;
-	flex-direction: column;
-}
+    <!-- Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
 
+    <!-- Styles -->
+    <style>
+        .title {
+            display: flex;
+            flex-direction: column;
+        }
+        .title > img {
+            max-width: 300px;
+        }
+    </style>
+</head>
 
-	</style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
+<body>
+    <div class="flex-center position-ref full-height">
+        @if (Route::has('login'))
+        <div class="top-right links">
+            @auth
+            <a href="{{ url('/home') }}">Home</a> @else
+            <a href="{{ route('login') }}">Login</a> @if (Route::has('register'))
+            <a href="{{ route('register') }}">Register</a> @endif @endauth
+        </div>
+        @endif
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+        <div class="content">
+            <div class="title m-b-md">
+                <img src="/images/icon.png" alt="Logo ICON"> Insightful Community <br> We Will Back...
+            </div>
 
-            <div class="content">
-                <div class="title m-b-md">
-			<img src="/images/icon.png" alt="Logo ICON">	
-			Insightful Community <br>
-We Will Back...
-                </div>
-
-                <div class="links">
-                    <a href="#">Facebook</a>
-                    <a href="#">Instagram</a>
-                </div>
+            <div class="links">
+                <a href="#">Facebook</a>
+                <a href="#">Instagram</a>
             </div>
         </div>
-    </body>
+    </div>
+</body>
+
 </html>
